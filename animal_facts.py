e71c3c4f147a3ec24c651f8cf9413a9749aa97a7
enungohotel <129783271+enungohotel@users.noreply.github.com>
2023-04-03 20:34:28 +0300

    Create animal_facts.py

---
+++ animal_facts.py
@@ -0,0 +1,6 @@
+import random
+
+facts = ["Elephants can't jump.", "A rhinoceros's horn is made of hair.", "Giraffes have the same number of neck vertebrae as humans.", "The blue whale is the largest animal on Earth."]
+
+fact = random.choice(facts)
+print(fact)
